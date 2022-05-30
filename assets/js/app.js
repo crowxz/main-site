5bc921878458aea016988f9ae48f12369c42fa83
Lucas Leite <82832236+devlucasleite@users.noreply.github.com>
2022-05-30 16:06:30 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -131,7 +131,7 @@ let footer = $(`
                <form action="https://formsubmit.co/lucasleite.bv@gmail.com" method="POST">
      <input type="text" name="name" required>
      <input type="email" name="email" required>
-     <button type="submit">Send</button>
+     <button type="submit">Enviar</button>
 </form>
              </div>
           </div>
